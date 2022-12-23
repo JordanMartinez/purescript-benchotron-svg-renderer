8512b5d1d46be39cb4ee05d8f855cf97536b48a4
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-12-22 21:10:04 -0600

    Update style of coding to be more ESM-like

--- benchotronRenderer.js
+++ benchotronRenderer.js
@@ -1,35 +1,35 @@
 window.BenchotronRenderer = {}
   ; (function () {
 
-    var margin = { top: 40, right: 40, bottom: 60, left: 80 },
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+    const margin = { top: 40, right: 40, bottom: 60, left: 80 };
+    const width = 960 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
 
-    var x = d3.scale.linear()
+    const x = d3.scale.linear()
       .range([0, width]);
 
-    var y = d3.scale.linear()
+    const y = d3.scale.linear()
       .range([height, 0]);
 
-    var xAxis = d3.svg.axis()
+    const xAxis = d3.svg.axis()
       .scale(x)
       .orient("bottom");
 
-    var yAxis = d3.svg.axis()
+    const yAxis = d3.svg.axis()
       .scale(y)
       .orient("left")
-      .tickFormat(function (d) {
-        var scientific = d3.format('0.2e')
-        var fixed = d3.format('0.2f')
-        return d <= 0.01 ? scientific(d) : fixed(d)
+      .tickFormat((d) => {
+        const scientific = d3.format('0.2e')
+        const fixed = d3.format('0.2f')
+        return d <= 0.01 ? scientific(d) : fixed(d);
       });
 
-    var line = d3.svg.line()
-      .x(function (d) { return x(d.size); })
-      .y(function (d) { return y(d.stats.mean); });
+    const line = d3.svg.line()
+      .x((d) => x(d.size))
+      .y((d) => y(d.stats.mean));
 
     function takeWhere(arr, pred) {
-      var result = arr.filter(pred);
+      const result = arr.filter(pred);
       if (result.length === 1) {
         return result[0];
       } else {
@@ -42,15 +42,15 @@ window.BenchotronRenderer = {}
     }
 
     function renderSeries(svg, seriesIndex, seriesLength, data) {
-      var series = svg.append("g")
+      const series = svg.append("g")
         .attr("class", dataSeriesClass(seriesIndex, seriesLength));
 
       series.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
-        .attr("cx", function (d) { return x(d.size) })
-        .attr("cy", function (d) { return y(d.stats.mean) })
+        .attr("cx", (d) => x(d.size))
+        .attr("cy", (d) => y(d.stats.mean))
         .attr("r", 3);
 
       series.selectAll("path.error-bar")
@@ -58,18 +58,18 @@ window.BenchotronRenderer = {}
         .enter()
         .append("path")
         .attr("class", "error-bar")
-        .attr("x", function (d) { return x(d.size) })
-        .attr("y", function (d) { return y(d.stats.mean) })
+        .attr("x", (d) => x(d.size))
+        .attr("y", (d) => y(d.stats.mean))
         .attr("d", function (d) {
-          var mx = x(d.size),
-            my = y(d.stats.mean),
-            my1 = y(d.stats.mean + d.stats.deviation),
-            my2 = y(d.stats.mean - d.stats.deviation),
-            dx = 3
+          const mx = x(d.size);
+          const my = y(d.stats.mean);
+          const my1 = y(d.stats.mean + d.stats.deviation);
+          const my2 = y(d.stats.mean - d.stats.deviation);
+          const dx = 3;
           return ["M", mx, my,
             "V", my1, "h", -dx, "h", 2 * dx, "h", -dx,
             "V", my2, "h", -dx, "h", 2 * dx
-          ].join(' ')
+          ].join(' ');
         })
 
       series.append("path")
@@ -79,54 +79,54 @@ window.BenchotronRenderer = {}
     }
 
     function concatArray(x, y) {
-      return x.concat(y)
+      return x.concat(y);
     }
 
     function drawGraph(data, elId) {
       // Delete the old graph
-      d3.select(elId + " svg").remove()
+      d3.select(elId + " svg").remove();
 
       // Create the new graph
-      var svg = d3.select(elId).append("svg")
+      const svg = d3.select(elId).append("svg");
 
       // Add the styles
-      var styleText = d3.select("#svg-styles").text()
+      const styleText = d3.select("#svg-styles").text();
       d3.select(elId + " svg")
         .append("defs")
         .append("style")
         .attr("type", "text/css")
-        .text(styleText)
+        .text(styleText);
 
       // Prepare the graph
-      var totalWidth = width + margin.left + margin.right;
-      var totalHeight = height + margin.top + margin.bottom;
+      const totalWidth = width + margin.left + margin.right;
+      const totalHeight = height + margin.top + margin.bottom;
       svg.attr("width", totalWidth)
-        .attr("height", totalHeight)
+        .attr("height", totalHeight);
       svg.append("rect")
         .attr("width", totalWidth)
         .attr("height", totalHeight)
-        .attr("class", "background")
-      svg = svg.append("g")
+        .attr("class", "background");
+      const chartArea = svg.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
       // Compute appropriate scales
-      var allData = data.series
-        .map(function (x) { return x.results })
-        .reduce(concatArray, []) // flatten
+      const allData = data.series
+        .map((x) => x.results)
+        .reduce(concatArray, []); // flatten
 
-      x.domain(d3.extent(allData, function (d) { return d.size }))
-      y.domain(d3.extent(allData, function (d) { return d.stats.mean }))
+      x.domain(d3.extent(allData, (d) => d.size));
+      y.domain(d3.extent(allData, (d) => d.stats.mean));
 
       // Draw title
-      svg.append("g")
+      chartArea.append("g")
         .attr("transform", "translate(" + (width / 2) + ", 0)")
         .append("text")
         .style("text-anchor", "middle")
         .style("font-size", "28px")
-        .text(data.title)
+        .text(data.title);
 
       // Draw x axis
-      svg.append("g")
+      chartArea.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis)
@@ -137,7 +137,7 @@ window.BenchotronRenderer = {}
         .text(data.sizeInterpretation);
 
       // Draw y axis
-      svg.append("g")
+      chartArea.append("g")
         .attr("class", "y axis")
         .call(yAxis)
         .append("text")
@@ -149,41 +149,41 @@ window.BenchotronRenderer = {}
 
       // Plot each data series
       data.series.map(function (d, index) {
-        renderSeries(svg, index, data.series.length, d.results);
+        renderSeries(chartArea, index, data.series.length, d.results);
       })
 
       // Draw legend
-      var legend = svg.append("g")
+      const legend = chartArea.append("g")
         .attr("class", "legend")
-        .attr("transform", "translate(50, 50)")
+        .attr("transform", "translate(50, 50)");
 
-      var legendData = data.series.map(function (d) { return d.name })
+      const legendData = data.series.map((d) => d.name);
       legend.selectAll("rect")
         .data(legendData)
         .enter()
         .append("rect")
         .attr("x", 0)
-        .attr("y", function (_, i) { return i * 20 })
+        .attr("y", (_, i) => i * 20)
         .attr("width", 15)
         .attr("height", 15)
-        .attr("class", function (_, i) { return dataSeriesClass(i, data.series.length); })
+        .attr("class", (_, i) => dataSeriesClass(i, data.series.length));
 
       legend.selectAll("text")
         .data(legendData)
         .enter()
         .append("text")
         .attr("x", 24)
-        .attr("y", function (_, i) { return i * 20 + 13 })
-        .text(function (d) { return d })
+        .attr("y", (_, i) => i * 20 + 13)
+        .text((d) => d);
 
       // HACK: Apply some classes for styling, since apparently lots of SVG
       // software can't cope with anything other than the most basic selectors
       svg.selectAll('.axis line')
-        .attr('class', 'axis-line')
+        .attr('class', 'axis-line');
 
       svg.selectAll('.axis path')
-        .attr('class', 'axis-path')
+        .attr('class', 'axis-path');
     }
 
-    window.BenchotronRenderer.drawGraph = drawGraph
-  })()
+    window.BenchotronRenderer.drawGraph = drawGraph;
+  })();
